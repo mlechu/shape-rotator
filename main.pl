33cd297105bb7e374f50c3863980c0972cc70aa3
Em Chu <im.mlechu@gmail.com>
2023-03-31 15:21:13 -0700

    Add list utils, shape representation; update readme

--- main.pl
+++ main.pl
@@ -12,35 +12,29 @@ p_(x, point(X,_,_), X).
 p_(y, point(_,Y,_), Y).
 p_(z, point(_,_,Z), Z).
 
+%% Bind a point to a name (makes code shorter)
+p_def(point(X, Y, Z), point(X, Y, Z)).
+
 p_mm(point(X,Y,Z), mat(A1,A2,A3,B1,B2,B3,C1,C2,C3), point(X2,Y2,Z2)) :-
     X2 is X * A1 + Y * A2 + Z * A3,
     Y2 is X * B1 + Y * B2 + Z * B3,
     Z2 is X * C1 + Y * C2 + Z * C3.
 
-hello :- point(1,2,3).
+gratio(GR) :- GR is (1 + sqrt(5)) / 2.
 
 %% reflect(x, point(X1,Y, Z),  point(X2,Y, Z))  :- X2 is X1 * -1.
 %% reflect(y, point(X, Y1,Z),  point(X, Y2,Z))  :- Y2 is Y1 * -1.
 %% reflect(z, point(X, Y, Z1), point(X, Y, Z2)) :- Z2 is Z1 * -1.
 
 
-%% tetrahedron :- shape([])
-%% cube :- shape([vertex(point( 1, 1, 1),[]),
-%%                vertex(point( 1, 1,-1),[]),
-%%                vertex(point( 1,-1, 1),[]),
-%%                vertex(point( 1,-1,-1),[]),
-%%                vertex(point(-1, 1, 1),[]),
-%%                vertex(point(-1, 1,-1),[]),
-%%                vertex(point(-1,-1, 1),[]),
-%%                vertex(point(-1,-1,-1),[])
-%%               ]).
-%% octahedron :- shape([])
-%% dodecahedron :- shape([])
-%% icosahedron :- shape([])
+%% str_to_l(S, L) :- L is the list of characters in string S
+str_to_l(S, L) :-
+    atom_codes(A, S),
+    atom_chars(A, L).
 
 %% l_append(A, B, AB) :- AB is the concatenation of the two lists
 l_append([H|A], B, [H|AB]) :-
-    list_append(A,B,AB).
+    l_append(A,B,AB).
 l_append([], B, B).
 
 %% l_last(L, El) :- El is the last element of L
@@ -48,20 +42,77 @@ l_last([El], El).
 l_last([_|T], El) :- last(T, El).
 
 %% reverse(A, B) :- A and B are equivalent but reversed
-%% reverse([], []).
-%% reverse([H|T], [H2|T2]) :-
+l_reverse([], []).
+l_reverse([H1|T1], L2) :-
+    l_reverse(T1, RT1),
+    l_append(RT1, [H1], L2).
 
-%% generate_shape(Str).
+%% Generate a shape from a seed (letter)
+%% Supports all 5 platonic solids
+%%
+%% TODO
+%% tetrahedron
+%% octahedron
+%% dodecahedron
+%% icosahedron
+%%
+%% p_def might be inefficent here
+s_seed("C", % Cube
+       shape([vertex(AAA,[BAA, ABA, AAB]),
+              vertex(AAB,[BAB, ABB, AAA]),
+              vertex(ABA,[BBA, AAA, ABB]),
+              vertex(ABB,[BBB, AAB, ABA]),
+              vertex(BAA,[AAA, BBA, BAB]),
+              vertex(BAB,[AAB, BBB, BAA]),
+              vertex(BBA,[ABA, BBA, BBB]),
+              vertex(BBB,[ABB, BAB, BBA])])) :-
+    p_def(AAA, point( 1, 1, 1)),
+    p_def(AAB, point( 1, 1,-1)),
+    p_def(ABA, point( 1,-1, 1)),
+    p_def(ABB, point( 1,-1,-1)),
+    p_def(BAA, point(-1, 1, 1)),
+    p_def(BAB, point(-1, 1,-1)),
+    p_def(BBA, point(-1,-1, 1)),
+    p_def(BBB, point(-1,-1,-1)).
 
+%% TODO
+%%
+%% irreducible: ambo, dual, gyro, join, kis, snub, truncate
+%%
+%% easy: bevel (ta), expand (aa), meta (kj), ortho (jj)
+%%
+%% maybe (in addition to john's set): reflect, propellor, needle, zip
+apply_op(S1, d, S).
+
+
+%% TODO allow for exponents on operators?
+apply_ops(S, [], S).
+apply_ops(S0, [Op|Ops], S) :-
+    apply_op(S0, Op, S1),
+    apply_ops(S1, Ops, S).
+
+to_svg(S, SVG).
+
+to_stl(S, STL).
+
+%% write_out(Filepath, Data) :- write Data to ./out/<Filepath>
 write_out(Filepath, Data) :-
     \+ exists_directory("out"),
     make_directory("out"),
     write_out(Filepath, Data).
-
 write_out(Filepath, Data) :-
     atom_concat("out/", Filepath, Path),
     open(Path, write, Fd, [create([default])]),
     write(Fd, Data),
     close(Fd).
+
+generate_shape(Str, SVG) :-
+    str_to_l(Str, Chars),
+    l_reverse(Chars, [Seed|Ops]),
+    s_seed(Seed, S0),
+    apply_ops(S0, Ops, S),
+    to_stl(S, STL),
+    write_out("shape.svg", SVG).
+
 
 main :- write_out("b.txt", "hello2").
--- readme.md
+++ readme.md
@@ -1,13 +1,25 @@
 # shape rotator
 
+## usage
+1. generate polyhedra from a seed (character representing platonic solid)
+    - e.g. "T" for tetrahedron
+2. modify seed polyhedron using John Conway polyhedron notation 
+    - e.g. "dkT" = (dual (kis tetrahedron))
+3. below are output options, from most to least likely of getting done:
+    - easiest would be ASCII STL; see resources
+    - lay out as a planar (polyhedral) graph in SVG format (this can be done with all outer edges as sides to a hidden face)
+    - project the shape onto a plane in SVG format 
+        - can get the user to specify a camera angle
+        - extra ambitious: interactive SVG option
+4. we can check graph isomorphism vs known polyhedra database add a caption to the output if there's a match
+
+## internals
+
+note: I'm currently using ISO prolog wherever possible instead of the swipl libraries. It might be wise to switch from swipl to gnu prolog. 
+
 - represent everything as a graph? 
     - Maybe a matrix would be more efficient, if I can figure out how the operators and their corresponding matrices on the wikipedia work
-- user can generate polyhedra from a seed (platonic solid)
-- user can modify polyhedra using john conway polyhedron notation
-- output:
-    - lay out as a planar graph in SVG format (this can be done with all outer edges as sides to a hidden face)
-    - maybe try for a 3d rendering, also SVG, if we get the user to specify a camera angle and we have time
-- we can check graph isomorphism vs known polyhedra database add a caption to the output if there's a match
+    - vertex configuration looks cool but unfortunately only works for uniform polyhedra. Perhaps still useful for the isomorphism check?
 
 ## resources
 
@@ -19,5 +31,11 @@
         - very easy: vertices, edges
         - maybe: faces, symmetry stats
 - online visualizer that already does a pretty excellent job: https://levskaya.github.io/polyhedronisme/
-
-I would not be surprised if something above promises to solve some sort of open problem in computer science lol. we'll figure out what features to add as we go.
+- 3D wireframes in SVG: https://prideout.net/blog/svg_wireframes/#examples
+- SVG blog posts (interactivity): https://www.petercollingridge.co.uk/search/?query=svg&page=1
+- STL might be easier than SVG: https://en.wikipedia.org/wiki/STL_(file_format)
+    - Snub cube example: https://en.wikipedia.org/wiki/Snub_cube
+- Projection onto the plane: https://en.wikipedia.org/wiki/Orthographic_projection
+- Polyhedral graph: https://en.wikipedia.org/wiki/Polyhedral_graph
+    - Do the numbers of faces, edges, and vertices of a convex polyhedron uniquely specify its graph? I would think no, but I can't find the answer. So probably no.
+- List of all Johnson solids (for later later): https://en.wikipedia.org/wiki/Johnson_solid
